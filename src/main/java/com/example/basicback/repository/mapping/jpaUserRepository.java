2c8c68b35f5c9f45996e6b1bd249074b734eaf99
Kim285-good <bar0805@naver.com>
2024-08-22 22:47:39 +0900

    BP-28 feat: 사용자 엔티티 및 레포지토리 생성

---
+++ src/main/java/com/example/basicback/repository/mapping/jpaUserRepository.java
@@ -0,0 +1,9 @@
+package com.example.basicback.repository.mapping;
+
+import com.example.basicback.model.pk.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface jpaUserRepository extends JpaRepository<User, String> {
+}
