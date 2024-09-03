fa849c0a5bb9260c53830e584c647fa3a921df37
Kim285-good <bar0805@naver.com>
2024-09-03 13:23:13 +0900

    BP-54 feat: 데이터베이스 모델 설계

---
+++ src/main/java/com/example/basicback/report/dto/response/ReportResponse.java
@@ -0,0 +1,17 @@
+package com.example.basicback.report.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ReportResponse {
+    private int rankNo;
+    private int reportId;
+    private String title;
+    private String content;
+    private String createId;
+    private String createAt;
+}
